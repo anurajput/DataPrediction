4a2bb042f7b8eef204ace3fa3fda4fda1740fd54
Anu <anurajput2017bytes@gmail.com>
2017-12-13 11:47:23 +0530

    plot the classifier

--- tests/regression_test.py
+++ tests/regression_test.py
@@ -4,6 +4,13 @@ import math
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
+import time
+
+style.use('ggplot')
+
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -36,26 +43,56 @@ df.fillna(-99999, inplace=True)
 forecast_out = int( math.ceil( 0.1 * len(df) ) )
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 print df.head()
 print df.tail()
 
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
-y = np.array(df['label'])
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
 
-print "X Len:", len(X)
-print "y Len:", len(y)
+df.dropna(inplace=True)
+
+y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
 #clf = LinearRegression()
 #clf = svm.SVR()
 #clf = svm.SVR(kernel='poly') # defualt kernel is linear
-clf = LinearRegression(n_jobs=10)
+#clf = LinearRegression(n_jobs=10)
+clf = LinearRegression()
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
 
 print "accuracy:", accuracy
+
+
+###############################
+#                             #
+# lets do some prediction now #
+#                             #
+###############################
+forecast_set = clf.predict(X_lately)
+
+print "forecast_set", forecast_set
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = time.mktime(last_date.timetuple())
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix+= one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
