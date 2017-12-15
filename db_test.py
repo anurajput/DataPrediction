1721cee0da71018301fc8a637bd029af789b1555
Anu Rajput <Anu Rajput>
2017-12-15 21:41:32 +0530

    updated files

--- db_test.py
+++ db_test.py
@@ -15,20 +15,24 @@ def _log(msg):
 def create_hekman():
     try:
         session = get_session()
-        query = session.query(Hekman).filter(Hekman.model_number ==
-                                             model_number).first()
-        _log('model_number: %s' % query.model_number)
-        _log('description: %s' % query.description)
-        _log('supply_for_days: %s' % query.supply_for_days)
-        _log('available_qty: %s' % query.available_qty)
-        _log('next_produce_qty: %s' % query.next_produce_qty)
-        _log('next_produce_date: %s' % query.next_produce_date)
-        _log('next_schedule_produce_qty: %s' % query.next_schedule_produce_qty)
-        _log('next_schedule_produce_date: %s' % query.next_schedule_produce_date)
-        _log('retail: %s' % query.retail)
-        _log('pricing: %s' % query.pricing)
+        query_record = session.query(Hekman).filter(Hekman.model_number ==
+                                                    model_number).all()
+        for query in query_record:
+
+            _log('model_number: %s' % query.model_number)
+            _log('description: %s' % query.description)
+            _log('supply_for_days: %s' % query.supply_for_days)
+            _log('available_qty: %s' % query.available_qty)
+            _log('next_produce_qty: %s' % query.next_produce_qty)
+            _log('next_produce_date: %s' % query.next_produce_date)
+            _log('next_schedule_produce_qty: %s' %
+                 query.next_schedule_produce_qty)
+            _log('next_schedule_produce_date: %s' %
+                 query.next_schedule_produce_date)
+            _log('retail: %s' % query.retail)
+            _log('pricing: %s' % query.pricing)
     except Exception as exp:
-        print("create_inventory() got exception: \n%s" % exp)
+        print("create_hekman() got exception: \n%s" % exp)
         print (traceback.format_exc())
 
 
